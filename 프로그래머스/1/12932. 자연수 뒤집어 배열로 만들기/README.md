14aba189dd08eb6a21695caa8184bf613b47fdf5
DanDanjoo <162088392+DanDanjoo@users.noreply.github.com>
2024-05-15 22:04:46 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 63.9 MB, 시간: 19.40 ms
+메모리: 62.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 17:41:40
+2024년 05월 15일 22:04:44
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -1,11 +1,10 @@
 class Solution {
     fun solution(n: Long): IntArray {
-        return n
-            .toString()
-            .toCharArray()
-            .reversed()
-            .map{ it.toString().toInt()}
-            .toIntArray()
+        val str = n.toString()
+        val result = IntArray(str.length)
+        for (i in str.indices) {
+            result[i] = str[str.length - 1 - i] - '0'
+        }
+        return result
     }
-
 }
